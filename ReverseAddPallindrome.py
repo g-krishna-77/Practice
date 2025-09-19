72171596ae2ae45e2a522e1a98441ecff30ec739
Krishna <krishnagokuldas77@gmail.com>
2025-09-19 15:47:55 +0530

    ReverseAddPallindrome.py

--- ReverseAddPallindrome.py
+++ ReverseAddPallindrome.py
@@ -1,11 +1,14 @@
 n = int(input())
-k = int(input())
+k = int(Input())
 
 for i in range(1, k+1):
-    n = n + int(str(n)[::-1])   # add reverse
-    if str(n) == str(n)[::-1]:  # check palindrome
+    rev_n = int(str(n)[::-1])   # reverse the digits
+    n = n + rev_n               # add reversed number
+    s = str(n)
+    if s == s[::-1]:            # check palindrome
         print([i, n])
         break
 else:
     print([-1, -1])
+
 
